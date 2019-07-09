9b394d10dd58e235461b4c2f793ba1b57fcedcc5
cnds <cleiton.de-moraes@gft.com>
2019-07-09 14:29:26 -0300

    First Commit and Add Project

---
+++ Tests/TheorySample.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class TheorySample
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
